e9e96e23fecd8458e4c52456c531968bcd28f0f3
Dejdzil <acdcars.filip@gmail.com>
2024-06-30 14:41:13 +0200

    Szybka poprawaV2

--- StN/Assets/Skrypty/Broń&Ikony/Book/Book.cs
+++ StN/Assets/Skrypty/Broń&Ikony/Book/Book.cs
@@ -7,28 +7,8 @@ public class Book : MonoBehaviour
     public float rotateSpeed;
     public WeaponsScriptableObject weaponData;
 
-    //Ustaw w prefabie broni pociskowej
-    protected Vector3 kierunki;
-    public float ZniszczPoSekundzie;
-
     public Transform Skeleton;
 
-    protected float currentDamage;
-    protected float currentSpeed;
-    protected float currentCooldownofweapon;
-    protected int currentWytrzymalocs;
-
-    private void Awake()
-    {
-        currentDamage = weaponData.Damage;
-        currentSpeed = weaponData.Speed;
-        currentCooldownofweapon = weaponData.Cooldownofweapon;
-        currentWytrzymalocs = weaponData.Wytrzymalosc;
-    }
-    protected virtual void Start()
-    {
-        Destroy(gameObject, ZniszczPoSekundzie);
-    }
 
 
     void Update()
@@ -36,31 +16,4 @@ public class Book : MonoBehaviour
         Skeleton.rotation = Quaternion.Euler(0f, 0f, Skeleton.rotation.eulerAngles.z + (rotateSpeed + Time.deltaTime));
     }
 
-    protected virtual void OnTriggerEnter2D(Collider2D col)
-    {
-        //gdy kolizja zadaj obrazenia
-        if (col.CompareTag("Enemy"))
-        {
-            EnemyStats enemy = col.GetComponent<EnemyStats>();
-            enemy.TakeDamage(currentDamage);
-            ZmniejszWytrzymalosc();
-        }
-        else if (col.CompareTag("Prop"))
-        {
-            if (col.gameObject.TryGetComponent(out BreakableProps breakable))
-            {
-                breakable.TakeDamage(currentDamage);
-                ZmniejszWytrzymalosc();
-            }
-        }
-    }
-
-    void ZmniejszWytrzymalosc() //niszczy pocisk gdy wytrzymalosc spada do 0
-    {
-        currentWytrzymalocs--;
-        if (currentWytrzymalocs <= 0)
-        {
-            Destroy(gameObject);
-        }
-    }
 }
--- StN/Assets/Skrypty/Broń&Ikony/Book/BookControl.cs
+++ StN/Assets/Skrypty/Broń&Ikony/Book/BookControl.cs
@@ -2,19 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BookControl : Weapons
+public class BookControl : MonoBehaviour
 {
+    public WeaponsScriptableObject weaponData;
 
-    protected override void Start()
+    public float destroyAfterSeconds;
+
+    protected float currentDamage;
+    protected float currentSpeed;
+    protected float currentCooldownofweapon;
+    protected int currentWytrzymalocs;
+
+    private void Awake()
     {
-        base.Start();
+        currentDamage = weaponData.Damage;
+        currentSpeed = weaponData.Speed;
+        currentCooldownofweapon = weaponData.Cooldownofweapon;
+        currentWytrzymalocs = weaponData.Wytrzymalosc;
     }
 
 
-    protected override void Attack()
+    protected virtual void Start()
     {
-        base.Attack();
-        GameObject spawnedKnife = Instantiate(weaponData.Prefab);
-        
+        Destroy(gameObject, destroyAfterSeconds);
+    }
+
+
+    protected virtual void OnTriggerEnter2D(Collider2D col)
+    {
+        //gdy kolizja zadaj obrazenia
+        if (col.CompareTag("Enemy"))
+        {
+            EnemyStats enemy = col.GetComponent<EnemyStats>();
+            enemy.TakeDamage(currentDamage);
+
+        }
     }
 }
